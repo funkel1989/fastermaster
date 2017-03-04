378dbb89acc7f3950488b3f2a01387def04bf74f
Michael Cavanaugh <funkel1989@gmail.com>
2017-03-04 12:18:06 -0500

    created class file helper

---
+++ FasterMaster/FasterMaster/FileHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FasterMaster
+{
+    class FileHelper
+    {
+        public static void GetDirectoriesAndFiles(string sourceDirectory, List<string> directories, List<string> files)
+        {
+            // Store a stack of our directories.
+            Stack<string> stack = new Stack<string>();
+
+            // Add initial directory.  prevents deleting temp folder
+            stack.Push(sourceDirectory);
+
+            while (stack.Count > 0)
+            {
+                // Get top directory
+                string dir = stack.Pop();
+
+                try
+                {
+                    // Add all files at this directory to the result List.
+                    files.AddRange(Directory.GetFiles(dir, "*.*"));
+
+                    // Add all directories at this directory.
+                    foreach (string dn in Directory.GetDirectories(dir))
+                    {
+                        stack.Push(dn);
+                        directories.Add(dn);
+                    }
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    //skip file
+                    Console.WriteLine("fail1");
+                }
+            }
+        }
+    }
+}
