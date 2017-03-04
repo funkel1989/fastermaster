d6d8afe6faef43d2b98f5c1ff49999afa1fa1b92
Michael Cavanaugh <funkel1989@gmail.com>
2017-03-04 12:17:11 -0500

    created program file

--- FasterMaster/FasterMaster/Program.cs
+++ FasterMaster/FasterMaster/Program.cs
@@ -1,15 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 
 namespace FasterMaster
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
+            RestartExplorer();
+            SetPCIdle();
+            DeleteTempFiles();
+
+            System.GC.Collect();
+            Console.ReadLine();
+        }
+
+
+
+        static void RestartExplorer()
+        {
+            foreach (Process p in Process.GetProcessesByName("explorer"))
+            {
+                p.Kill();
+            }
+        }
+
+        static void SetPCIdle()
+        {
+            ProcessStartInfo setIdle = new ProcessStartInfo();
+            setIdle.FileName = @"C:\windows\SysWOW64\rundll32.exe";
+            setIdle.Arguments = @"advapi32.dll ProcessIdleTasks";
+            Process.Start(setIdle);
+        }
+
+
+
+        static void DeleteTempFiles()
+        {
+            string folderPath = string.Empty;
+            folderPath = System.Environment.GetEnvironmentVariable("temp");
+
+            List<string> directories = new List<string>();
+            List<string> files = new List<string>();
+            string sourceDir = folderPath;
+
+            FileHelper.GetDirectoriesAndFiles(sourceDir, directories, files);
+
+
+            foreach (string file in files)
+            {
+                try
+                {
+                    System.IO.File.Delete(file);
+                }
+                catch { }
+
+
+            }
+
+            foreach (string directory in directories)
+            {
+                try
+                {
+                    Directory.Delete(directory);
+                }
+                catch { }
+
+            }
         }
     }
 }
